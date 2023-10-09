3805a60450cfe9d126a3bf65d1f293196272b9dc
qperez <quentin.perez@inria.fr>
2023-10-09 10:40:21 +0200

    Initial commit

---
+++ src/main/java/fr/insa/cours/perez/devops/nombre/imaginaire/NombreImaginaire.java
@@ -0,0 +1,9 @@
+package fr.insa.cours.perez.devops.nombre.imaginaire;
+
+import fr.insa.cours.perez.devops.nombre.reel.Nombre;
+
+public class NombreImaginaire extends Nombre {
+    public NombreImaginaire(Double valeur) {
+        super(valeur);
+    }
+}
---
+++ src/test/java/fr/insa/cours/perez/devops/nombre/reel/TestClass.java
@@ -0,0 +1,29 @@
+package fr.insa.cours.perez.devops.nombre.reel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestClass {
+    public Nombre nombre;
+
+    @BeforeEach
+    public void setUp(){
+        Double a =nombre.diviser(23.0);
+    }
+
+
+    @Test
+    public void testDivPar0(){
+        Nombre nombre = new Nombre(42d);
+        assertThrows(NumberFormatException.class, ()-> nombre.diviser(0d));
+    }
+
+    @Test
+    public void testDivParNan(){
+        Nombre nombre = new Nombre(42d);
+        assertThrows(NumberFormatException.class, ()-> nombre.diviser(Double.NaN),
+                "Exception Message");
+    }
+}
